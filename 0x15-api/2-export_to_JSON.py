034631c9d314176c4fbf6c032ad4fcf746827156
john mokaya <mokayaj857@gmail.com>
2024-07-29 23:40:52 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,6 @@
 
 #!/usr/bin/python3
-
-"""Should exports to-do list information for a given employee ID to JSON format."""
-
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
 import sys
