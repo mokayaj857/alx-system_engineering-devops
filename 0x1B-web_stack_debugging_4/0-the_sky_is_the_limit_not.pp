2f1ca611a97ba363478599f9c540f088f72d0e71
john mokaya <mokayaj857@gmail.com>
2024-08-22 21:47:54 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 #I should increase the amount of traffic an Nginx server can handle.
 
-# Increasing the ULIMIT of the default file
+# Increasing the ULIMIT of the default files
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
